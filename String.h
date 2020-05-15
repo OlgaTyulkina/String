19fea216f71eb8ff727721575bf3df1a3291b159
OlgaTyulkina <61185779+OlgaTyulkina@users.noreply.github.com>
2020-05-15 13:28:46 +0300

    Update String.h

--- String.h
+++ String.h
@@ -10,13 +10,9 @@ private:
 	int size;
 public:
 	TString();
-
 	TString(char b, int leng);
-
 	TString(const char* tstr);
-
 	TString(TString& a);
-
 	~TString();
 
 	char* get_s()
@@ -28,31 +24,18 @@ public:
 		return length;
 	}
 	void set_s(const char* s);
-
 	friend TString operator +(TString& a, TString& b);
-
 	TString& operator =(const TString& a);
-
 	void complete(TString& a, TString& b);
-
 	friend bool operator == (TString& a, TString& b);
-
 	friend bool operator <(TString& a, TString& b);
-
 	friend bool operator >(TString& a, TString& b);
-
 	char& operator [](const int index);
-
 	friend ostream& operator <<(ostream& out, TString& a);
-
 	friend istream& operator >> (istream& in, TString& b);
-
 	char* find(const char* e);
-
 	char* find_c(const char* c);
-
 	TString* Tstrtok(const char* c);
-
 	TString repeater(TString& s, int n);
 
 };
